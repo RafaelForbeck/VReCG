bd01b99457c19517e45b8a41bc10092f3c307d18
Rafael Forbeck <rafaelforbeck@gmail.com>
2024-04-25 21:26:14 -0300

    Adiciona funcionalidade de desenho por pixel

--- Assets/Scripts/Matrix.cs
+++ Assets/Scripts/Matrix.cs
@@ -5,36 +5,75 @@ using UnityEngine;
 public class Matrix : MonoBehaviour
 {
     public GameObject cellModel;
+    public int colCount, rowCount;
 
-    List<Cell> cells = new List<Cell>();
+    public Color color;
+
+    List<List<Cell>> cells = new List<List<Cell>>();
 
     void Start()
     {
-        for (int row = 0; row < 10; row++)
+        for (int row = 0; row < rowCount; row++)
         {
-            for (int col = 0; col < 10; col++)
+            var line = new List<Cell>();
+
+            for (int col = 0; col < colCount; col++)
             {
                 var cellGO = Instantiate(cellModel, new Vector3(col, row), Quaternion.identity, transform);
                 var cell = cellGO.GetComponent<Cell>();
-                cells.Add(cell);
+                cell.TurnOff();
+                ChangeColor(cell);
+                line.Add(cell);
             }
+            cells.Add(line);
         }
+    }
 
-        
+    void ChangeColor(Cell cell)
+    {
+        var r = Random.Range(0f, 1f);
+        var g = Random.Range(0f, 1f);
+        var b = Random.Range(0f, 1f);
 
+        var color = new Color(r, g, b);
 
+        cell.spriteRenderer.color = color;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            //cell.TurnOn();
+            var cell = GetCell(Input.mousePosition);
+            if (cell != null)
+            {
+                cell.TurnOn();
+                cell.spriteRenderer.color = color;
+            }
         }
 
-        if (Input.GetMouseButtonDown (1))
+        if (Input.GetMouseButton(1))
         {
-            //cell.TurnOff();
+            var cell = GetCell(Input.mousePosition);
+            if (cell != null)
+            {
+                cell.TurnOff();
+            }
         }
+    }
+
+    Cell GetCell(Vector3 mousePos)
+    {
+        var coord = Camera.main.ScreenToWorldPoint(mousePos);
+        int col = Mathf.RoundToInt(coord.x);
+        int row = Mathf.RoundToInt(coord.y);
+
+        if (col < 0 || row < 0 || col >= colCount || row >= rowCount)
+        {
+            return null;
+        }
+
+        var cell = cells[row][col];
+        return cell;
     }
 }
